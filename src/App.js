369c0e88eb90971528e33ecb98ff4024d5b51ff0
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-07 17:09:27 +0200

    Fix title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 import Index from './pages/index'
 
 function App() {
+
+  useEffect(() => {
+    const title = `Light-Up | Everything's under controls`;
+    document.title = title
+  }, [])
+
+
   return (
     <>
       <Index />
