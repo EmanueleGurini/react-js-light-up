59a840f4d2066cef81fbe86e50b207976a3db3d9
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-07 12:28:13 +0200

    Add Hero Component

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import styles from './Hero.module.css'
+
+const Hero = () => {
+	return (
+		<section className={styles.hero}>
+			<h1>Light-Up: everything's under control</h1>
+		</section>
+	)
+}
+
+export default Hero;
