63803d3c2b292cf717687730bbb08861ae6d42b3
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-09 22:55:48 +0200

    Set Search component with with onClick function

--- src/components/home/search/Search.jsx
+++ src/components/home/search/Search.jsx
@@ -1,23 +1,31 @@
-import styles from './Search.module.css'
+import { useRef } from "react";
+
+import styles from "./Search.module.css";
 import IconLens from "../../../assets/icons/icon-lens.svg";
 
-const Search = () => {
-	return (
-		<section className={styles.container}>
-			<div className={styles.search}>
-			<div className={styles["search__left"]}>
-        <img src={IconLens} alt="Icon Lens" />
-        <input
-          type="text"
-          placeholder="Enter the URI here.."
-          onChange={() => console.log('isChanging')}
-          className={styles.input}
-        />
-      </div>
-      <button onClick={() => console.log('clicked')}>Search</button>
-			</div>
-		</section>
-	)
-}
+const Search = ({ onClick }) => {
+  const input = useRef(null);
 
-export default Search
+  const handleClick = () => {
+    onClick(input.current.value);
+  };
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.search}>
+        <div className={styles["search__left"]}>
+          <img src={IconLens} alt="Icon Lens" />
+          <input
+            type="text"
+            placeholder="Enter the URI here.."
+            className={styles.input}
+            ref={input}
+          />
+        </div>
+        <button onClick={handleClick}>Search</button>
+      </div>
+    </section>
+  );
+};
+
+export default Search;
