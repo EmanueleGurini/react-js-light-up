c08f19c05e9f7ed5032b91fbe0581e36d92216ff
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-09 22:57:12 +0200

    Setup Index with Redux mapStateToProps and mapDispatchToProps

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,15 +1,35 @@
-import Hero from './hero/Hero'
-import Search from './search/Search'
-import Result from './result/Result'
+import { connect } from "react-redux";
+import { fetchDataFromGoogleAPI } from "../../redux/actions.js";
 
-const Home = () => {
-	return (
-		<>
-			<Hero />
-			<Search />
-			<Result />
-		</>
-	)
-}
+import Hero from "./hero/Hero";
+import Search from "./search/Search";
+import Result from "./result/Result";
+import Spinner from "../shared/spinner/Spinner";
 
-export default Home; 
+const Home = ({ updateURL, googleAPIResult, fetchDataFromGoogleAPI }) => {
+  const { loading, data, error } = googleAPIResult;
+
+  return (
+    <>
+      <Hero />
+      <Search onClick={fetchDataFromGoogleAPI} />
+      {loading && <Spinner />}
+      {data && <Result />}
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    googleAPIResult: state.data,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchDataFromGoogleAPI: (urlValue) =>
+      dispatch(fetchDataFromGoogleAPI(urlValue)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
