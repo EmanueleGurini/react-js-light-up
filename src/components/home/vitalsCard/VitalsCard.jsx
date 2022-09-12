00349fa6c3644935c874d0da52c4f63e0f78e767
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-12 05:37:12 +0200

    Updates dynamics data inside VitalsCardCompnent

--- src/components/home/vitalsCard/VitalsCard.jsx
+++ src/components/home/vitalsCard/VitalsCard.jsx
@@ -1,11 +1,11 @@
 import styles from "./VitalsCard.module.css";
 
-const VitalsCard = () => {
+const VitalsCard = ({ data }) => {
   return (
     <div className={styles.container}>
       <div className={`${styles["vitals-card"]}`}>
-        <h3>Cumulative Layout Shift</h3>
-        <p>0.79 S</p>
+        <h3>{data?.name}</h3>
+        <p>{data?.value}</p>
       </div>
     </div>
   );
