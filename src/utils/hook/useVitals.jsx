a61642b3eb14ff5192d9c095f659aa2f247d091d
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-12 05:38:02 +0200

    Add useVitals hook to extract data

---
+++ src/utils/hook/useVitals.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+const useVitals = ({ data }) => {
+  const [vitals, setVitals] = useState([]);
+
+  useEffect(() => {
+    setVitals([
+      {
+        name: "Cumulative Layout Shift",
+        value:
+          data?.records[0]?.fields?.data?.["cumulative-layout-shift"]
+            ?.displayValue,
+      },
+      {
+        name: "Speed Index",
+        value: data?.records[0]?.fields?.data?.["speed-index"]?.displayValue,
+      },
+      {
+        name: "First Contentful Paint",
+        value:
+          data?.records[0]?.fields?.data?.["first-contentful-paint"]
+            ?.displayValue,
+      },
+      {
+        name: "Time to Interactive",
+        value:
+          data?.records[0]?.fields?.data?.["time-to-interactive"]?.displayValue,
+      },
+      {
+        name: "Largest Contentful Paint",
+        value:
+          data?.records[0]?.fields?.data?.["largest-contentful-paint"]
+            ?.displayValue,
+      },
+      {
+        name: "Total Blocking Time",
+        value:
+          data?.records[0]?.fields?.data?.["total-blocking-time"]?.displayValue,
+      },
+    ]);
+  }, [data]);
+
+  return { vitals };
+};
+
+export default useVitals;
