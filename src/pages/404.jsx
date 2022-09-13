40c5515c790f561a0992161ba8fb148e27436971
emanuelegurini <emanuele.gurini89@gmail.com>
2022-09-13 09:22:12 +0200

    Add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout/index";
+
+const PageNotFound = () => {
+  return (
+    <Layout>
+      <h1>"Oh, Jeez!"</h1>
+      <h2>404</h2>
+    </Layout>
+  );
+};
+
+export default PageNotFound;
